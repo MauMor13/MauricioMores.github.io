f94c3249347aa4942b5927b635e9319b54bf5ff8
Mauricio <mauri.f.mores@gmail.com>
2023-08-22 19:19:35 -0300

    REFACTOR: change in CarouselTEchnologies

--- src/components/container/CarouselTechnologies.jsx
+++ src/components/container/CarouselTechnologies.jsx
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 import styled from 'styled-components';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-const ConteinCarousel = styled.div `
+const ConteinCarousel = styled.div`
     width: 100%;
     height: 20rem;
 `;
@@ -12,29 +12,50 @@ const CarouselTechnologies = () => {
     const containerRef = useRef(null);
 
     useEffect(() => {
+        const iconTechnologies = ['src/assets/image/iconTechnologies/iconBootstrap.svg',
+            'src/assets/image/iconTechnologies/iconCss3.svg',
+            'src/assets/image/iconTechnologies/iconGit.svg',
+            'src/assets/image/iconTechnologies/iconGithub.svg',
+            'src/assets/image/iconTechnologies/iconHtml5.svg',
+            'src/assets/image/iconTechnologies/iconJava.svg',
+            'src/assets/image/iconTechnologies/iconJira.svg',
+            'src/assets/image/iconTechnologies/iconJS.svg',
+            'src/assets/image/iconTechnologies/iconJS.svg',
+            'src/assets/image/iconTechnologies/iconReact.svg',
+            'src/assets/image/iconTechnologies/iconSpring.svg',
+            'src/assets/image/iconTechnologies/iconStiled.svg',
+            'src/assets/image/iconTechnologies/iconTrello.svg',
+            'src/assets/image/iconTechnologies/iconVue.svg'];
+
         const currentRef = containerRef.current;
         const { clientWidth: width, clientHeight: height } = currentRef;
-        
+
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
         scene.add(camera);
-        const renderer = new THREE.WebGLRenderer({ alpha: true });
+        const renderer = new THREE.WebGLRenderer({ alpha: true  });
         renderer.setSize(width, height);
         containerRef.current.appendChild(renderer.domElement);
 
         const controls = new OrbitControls(camera, renderer.domElement);
-        controls.enableDamping = false;
         controls.enableZoom = false;
-
-        const sphereGeometry = new THREE.SphereGeometry(5, 32, 32);
-        const texture =  new THREE.TextureLoader().load('src/assets/image/iconTechnologies/html5-logo-31815.png');
-        texture.colorSpace = THREE.SRGBColorSpace;
-        const sphereMaterial = new THREE.MeshBasicMaterial({ map: texture});
-        const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        scene.add(sphere);
-        camera.position.z = 10;
-        camera.lookAt(sphere.position);
-
+        controls.enableRotate = false;
+        controls.enablePan = false;
+        const spheres = [];
+        const sphereRadius = 5;
+        const sphereSeparation = 2;
+        iconTechnologies.forEach((iconTechnologie, index) => {
+            const sphereGeometry = new THREE.SphereGeometry(sphereRadius, 32, 32);
+            const texture =  new THREE.TextureLoader().load(iconTechnologie);
+            texture.colorSpace = THREE.SRGBColorSpace;
+            const sphereMaterial = new THREE.MeshBasicMaterial({ map : texture});
+            const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+            sphere.rotateY(30);
+            sphere.position.x = index * (sphereRadius * 2 + sphereSeparation);
+            scene.add(sphere);
+            spheres.push(sphere);
+        });
+        camera.position.z = 12;
 
         const animate = () => {
             controls.update();
