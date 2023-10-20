db1797e18683738e7cd7a8ca68552aa3dd50a14d
Mauricio <mauri.f.mores@gmail.com>
2023-10-20 17:58:54 -0300

    REFACTOR: change animation the angle

--- src/components/container/CarouselTechnologies.jsx
+++ src/components/container/CarouselTechnologies.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useEffect } from 'react';
 
 const Container = styled.div`
     display: flex;
@@ -6,35 +7,24 @@ const Container = styled.div`
     transform: rotateX(-4deg);
     transform-style: preserve-3d;
     position: relative;
-    contain: content;
     width: 100%;
     height: 24rem;
     transform-origin: 50% 50% -500px;
-    transition: 1s;
 `;
 
 const Icon = styled.img`
-    width: 8rem;
-    height: 8rem;
+    width: 10rem;
+    height: 10rem;
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
     position: absolute;
     left: 42%;
     transform-origin: 80% 50% -600px;
-    border-radius: 21px;
-    background: linear-gradient(145deg, #4f175675, #5e1c6779);
-    box-shadow:  20px 20px 60px #4b1652,
-            -20px -20px 60px #651e6e;
-    animation: changeAngle 20s linear infinite;
-
-    @keyframes changeAngle {
-        100%{
-            transform: rotateY(${(props) => {
-        return (props.angle < -360 ? 0 : props.angle - 24);
-    }}deg);
-        }
-    }
+    border-radius: 100px;
+    background: linear-gradient(155deg, #4b1652,#4f175675 );
+    box-shadow:  10px 10px 40px  #5e1c6779 ,
+            -10px -10px 70px #651e6e;
 `;
 const CarouselTechnologies = () => {
     const iconTechnologies = [
@@ -54,10 +44,16 @@ const CarouselTechnologies = () => {
         'src/assets/image/iconTechnologies/trello.svg',
         'src/assets/image/iconTechnologies/vue.svg'
     ];
+    useEffect(() => {
+        
+        return () => {
+            
+        };
+    }, []);
     return (
         <Container>
             {iconTechnologies.map((icon, index) => {
-                let angle = index * (-360 / iconTechnologies.length);
+                let angle = index * (360 / iconTechnologies.length);
                 return <Icon key={index} src={icon} angle={angle} style={{ transform: "rotateY(" + angle + "deg)" }}></Icon>
             })}
         </Container>
