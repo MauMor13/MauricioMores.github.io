d641a15f00655b583b2d924e9b736e6365d18f68
Mauricio <mauri.f.mores@gmail.com>
2023-10-30 10:23:51 -0300

    REFACTOR: Change text in proyects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -30,6 +30,7 @@ const Text = styled.p`
     font-size: 1.3rem;
     padding-top: 1rem;
     text-align: center;
+    min-height: 5rem;
 `;
 
 const ContainCards = styled.section`
@@ -99,18 +100,21 @@ const Logo = styled.img`
 const TitleCard = styled.h2`
     font-family: 'Blouse','Aesthetic','Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     text-align: center;
+    margin-top: 1rem;
 `;
 
 const TextCard = styled.p`
     font-family: 'Courier New', Courier, monospace;
-    display: block; 
     font-size: 1rem;
-    padding-top: 1rem;
+    height: 10rem;
+    padding: 0rem 1rem 0rem 1rem;
     text-align: center;
+    overflow: auto;
 `;
 
 const ButtonCard = styled.a`
     cursor: pointer;
+    margin-bottom: 1rem;
     text-decoration: none;
     padding: .3rem;
     border-radius: .5rem;
@@ -189,8 +193,12 @@ const Projects = () => {
                     <ContentCard>
                         <TitleCard>Diamond Bank</TitleCard>
                         <TextCard>
-                        Este fue un proyecto realizado de forma autonoma, el cual fue diseñado pensado en el usuario 
-                        
+                        I had the opportunity to develop a full-fledged web banking application, 
+                        incorporating technologies such as HTML, CSS, JavaScript, Vue.js, and 
+                        Bootstrap for the frontend, and Java with Spring for the backend. The 
+                        application features a client account system, online loan request and 
+                        approval functionality, among other features, in addition to an attractive 
+                        and responsive design.
                         </TextCard>
                         <ButtonCard href='https://github.com/MauMor13/Homebanking'>More . . .</ButtonCard>
                     </ContentCard>
@@ -200,11 +208,16 @@ const Projects = () => {
                     <ContentCard>
                         <TitleCard>Adstore Detailing</TitleCard>
                         <TextCard>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
-                        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, 
-                        dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper 
-                        congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est 
-                        eleifend mi, non fermentum diam nisl sit amet erat.
+                        This project was the result of the collaboration of a team of five people, 
+                        in which I had the honor of leading. It was developed for a company specializing 
+                        in vehicle washing and polishing, which also offers a wide range of cleaning and 
+                        maintenance products. We used various technologies to bring to life a highly aesthetic 
+                        and fully responsive website. The website includes features such as appointment 
+                        scheduling and inventory control for both services and products. This project 
+                        not only allowed us to showcase our technical skills but also our ability to 
+                        work as a team and lead a project from its conception to its implementation. 
+                        I am proud to have led this effort and delivered a comprehensive solution for 
+                        our client, enhancing their online presence and streamlining their business management.
                         </TextCard>
                         <ButtonCard href='https://github.com/MauMor13/Proyecto-Adstore-Detailing'>More . . .</ButtonCard>
                     </ContentCard>
@@ -214,11 +227,18 @@ const Projects = () => {
                     <ContentCard>
                         <TitleCard>Happy Paws</TitleCard>
                         <TextCard>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
-                        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, 
-                        dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper 
-                        congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est 
-                        eleifend mi, non fermentum diam nisl sit amet erat.
+                        I had the privilege of being part of a beautiful project carried out for a veterinary clinic. 
+                        We developed a visually appealing and distinctive website that followed a specific layout. 
+                        To efficiently manage our team´s tasks, we utilized the Trello platform, which established 
+                        an agile and collaborative work dynamic.
+
+                        Our focus was on creating an exceptional frontend. The vision was to transform the website into 
+                        an e-commerce platform for the sale of toy products and pharmaceutical items. Initially, we concentrated 
+                        on developing an attractive frontend with a user-friendly interface.
+
+                        In the next phase of the project, we plan to implement a robust backend with the necessary business logic. 
+                        This will enable us to manage product stock effectively and facilitate the payment process for sales 
+                        through online payment methods.
                         </TextCard>
                         <ButtonCard href='https://github.com/MauMor13/patitas_felices'>More . . .</ButtonCard>
                     </ContentCard>
@@ -228,11 +248,21 @@ const Projects = () => {
                     <ContentCard>
                         <TitleCard>Programming and Robotics</TitleCard>
                         <TextCard>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. 
-                        Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, 
-                        dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper 
-                        congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est 
-                        eleifend mi, non fermentum diam nisl sit amet erat.
+                        I am currently leading an educational project at a technical school that focuses on teaching the 
+                        fundamental concepts of robotics and programming using Arduino technology as the foundation. Through 
+                        a combination of practical tasks and theoretical lessons, we strive to transform these concepts into 
+                        the realization of small automated robots capable of collision-free movement, thanks to the use of 
+                        ultrasonic sensors.
+
+                        Our project also involves the demonstration and application of various other sensors and components, 
+                        temperature sensors, and infrared transmitters-receivers. These components not only enhance our 
+                        understanding of the technology but also pave the way for their integration into a wide range 
+                        of projects.
+
+                        This educational initiative not only equips students with vital skills in robotics and programming but 
+                        also fosters a hands-on, exploratory learning experience that ignites creativity and innovation. I am 
+                        proud to be part of this endeavor, guiding students towards a future enriched with technological knowledge 
+                        and possibilities.
                         </TextCard>
                         <ButtonCard href='src/assets/image/imgProyect/Programacion_y_Robotica.pdf' download={'Programacion_y_Robotica'}>More . . .</ButtonCard>
                     </ContentCard>
